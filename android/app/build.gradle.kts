6ab32f7cb37a29797b19f9126eb121c96e317584
Afin Arif Azyumardi <afinafin024@gmail.com>
2026-06-03 08:12:08 +0700

    build setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tigo_mobile"
+    namespace = "com.company.tigo_mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tigo_mobile"
+        applicationId = "com.company.tigo_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
